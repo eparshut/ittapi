5666ea05993defc3deacda7505051509c8ce0153
Parshutin, Eugeny <eugeny.parshutin@intel.com>
2026-04-21 08:33:05 -0500

    rename Domain class member

--- cpp/include/ittapi_domain.hpp
+++ cpp/include/ittapi_domain.hpp
@@ -22,155 +22,155 @@ class Domain
 {
 public:
     explicit Domain(std::string_view name)
-        : m_handle(detail::create_domain(std::string(name).c_str()))
+        : m_domain(detail::create_domain(std::string(name).c_str()))
     {
     }
 
 #if ITT_PLATFORM == ITT_PLATFORM_WIN
     explicit Domain(std::wstring_view name)
-        : m_handle(detail::create_domain(std::wstring(name).c_str()))
+        : m_domain(detail::create_domain(std::wstring(name).c_str()))
     {
     }
 #endif
 
     __itt_domain* native_handle() const noexcept
     {
-        return m_handle;
+        return m_domain;
     }
 
     bool valid() const noexcept
     {
-        return m_handle != nullptr;
+        return m_domain != nullptr;
     }
 
     Domain(const Domain&) = default;
     Domain& operator=(const Domain&) = default;
 
     Domain(Domain&& other) noexcept
-        : m_handle(other.m_handle)
+        : m_domain(other.m_domain)
     {
-        other.m_handle = nullptr;
+        other.m_domain = nullptr;
     }
 
     Domain& operator=(Domain&& other) noexcept
     {
-        m_handle = other.m_handle;
-        other.m_handle = nullptr;
+        m_domain = other.m_domain;
+        other.m_domain = nullptr;
         return *this;
     }
 
     [[nodiscard]] ScopedTask task(std::string_view name) const
     {
-        return ScopedTask(m_handle, name);
+        return ScopedTask(m_domain, name);
     }
 
     [[nodiscard]] ScopedTask task(std::string_view name, __itt_id taskid, __itt_id parentid) const
     {
-        return ScopedTask(m_handle, name, taskid, parentid);
+        return ScopedTask(m_domain, name, taskid, parentid);
     }
 
 #if ITT_PLATFORM == ITT_PLATFORM_WIN
     [[nodiscard]] ScopedTask task(std::wstring_view name) const
     {
-        return ScopedTask(m_handle, name);
+        return ScopedTask(m_domain, name);
     }
 
     [[nodiscard]] ScopedTask task(std::wstring_view name, __itt_id taskid, __itt_id parentid) const
     {
-        return ScopedTask(m_handle, name, taskid, parentid);
+        return ScopedTask(m_domain, name, taskid, parentid);
     }
 #endif
 
     [[nodiscard]] ScopedTask task(const StringHandle& name) const noexcept
     {
-        return ScopedTask(m_handle, name);
+        return ScopedTask(m_domain, name);
     }
 
     [[nodiscard]] ScopedTask task(const StringHandle& name, __itt_id taskid, __itt_id parentid) const noexcept
     {
-        return ScopedTask(m_handle, name, taskid, parentid);
+        return ScopedTask(m_domain, name, taskid, parentid);
     }
 
     void task_begin(std::string_view name) const
     {
         __itt_string_handle* h = detail::get_or_create_string_handle(name);
-        __itt_task_begin(m_handle, detail::make_null_id(), detail::make_null_id(), h);
+        __itt_task_begin(m_domain, detail::make_null_id(), detail::make_null_id(), h);
     }
 
     void task_begin(std::string_view name, __itt_id taskid, __itt_id parentid) const
     {
         __itt_string_handle* h = detail::get_or_create_string_handle(name);
-        __itt_task_begin(m_handle, taskid, parentid, h);
+        __itt_task_begin(m_domain, taskid, parentid, h);
     }
 
 #if ITT_PLATFORM == ITT_PLATFORM_WIN
     void task_begin(std::wstring_view name) const
     {
         __itt_string_handle* h = detail::get_or_create_string_handle(name);
-        __itt_task_begin(m_handle, detail::make_null_id(), detail::make_null_id(), h);
+        __itt_task_begin(m_domain, detail::make_null_id(), detail::make_null_id(), h);
     }
 
     void task_begin(std::wstring_view name, __itt_id taskid, __itt_id parentid) const
     {
         __itt_string_handle* h = detail::get_or_create_string_handle(name);
-        __itt_task_begin(m_handle, taskid, parentid, h);
+        __itt_task_begin(m_domain, taskid, parentid, h);
     }
 #endif
 
     void task_begin(const StringHandle& name) const noexcept
     {
-        __itt_task_begin(m_handle, detail::make_null_id(), detail::make_null_id(), name.native_handle());
+        __itt_task_begin(m_domain, detail::make_null_id(), detail::make_null_id(), name.native_handle());
     }
 
     void task_begin(const StringHandle& name, __itt_id taskid, __itt_id parentid) const noexcept
     {
-        __itt_task_begin(m_handle, taskid, parentid, name.native_handle());
+        __itt_task_begin(m_domain, taskid, parentid, name.native_handle());
     }
 
     void task_end() const noexcept
     {
-        __itt_task_end(m_handle);
+        __itt_task_end(m_domain);
     }
 
     [[nodiscard]] ScopedRegion region(std::string_view name) const
     {
-        return ScopedRegion(m_handle, name);
+        return ScopedRegion(m_domain, name);
     }
 
     [[nodiscard]] ScopedRegion region(std::string_view name, __itt_id id, __itt_id parentid) const
     {
-        return ScopedRegion(m_handle, name, id, parentid);
+        return ScopedRegion(m_domain, name, id, parentid);
     }
 
 #if ITT_PLATFORM == ITT_PLATFORM_WIN
     [[nodiscard]] ScopedRegion region(std::wstring_view name) const
     {
-        return ScopedRegion(m_handle, name);
+        return ScopedRegion(m_domain, name);
     }
 
     [[nodiscard]] ScopedRegion region(std::wstring_view name, __itt_id id, __itt_id parentid) const
     {
-        return ScopedRegion(m_handle, name, id, parentid);
+        return ScopedRegion(m_domain, name, id, parentid);
     }
 #endif
 
     [[nodiscard]] ScopedRegion region(const StringHandle& name) const noexcept
     {
-        return ScopedRegion(m_handle, name);
+        return ScopedRegion(m_domain, name);
     }
 
     [[nodiscard]] ScopedRegion region(const StringHandle& name, __itt_id id, __itt_id parentid) const noexcept
     {
-        return ScopedRegion(m_handle, name, id, parentid);
+        return ScopedRegion(m_domain, name, id, parentid);
     }
 
     [[nodiscard]] ScopedFrame frame() const noexcept
     {
-        return ScopedFrame(m_handle);
+        return ScopedFrame(m_domain);
     }
 
 private:
-    __itt_domain* m_handle = nullptr;
+    __itt_domain* m_domain = nullptr;
 };
 
 }  // namespace ittapi
